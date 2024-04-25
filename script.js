2d87393a300b182ecd6ff4909e681fa290937c11
shunsuke ikegami <ikegami.shunsuke@lib.bbiq.jp>
2024-04-26 04:19:34 +0900

    画面定数はviewにのみ。

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 import { BoardModel } from "./BoardModel.js";
 import { Controller } from "./Controller.js";
 
-const GAMESIZE = 300;
-const CELLSIZE = GAMESIZE / 8;
-const CELL_INNER_SIZE = CELLSIZE * 0.98;
-const CENTER_POSTION_OF_CIRCLE = CELL_INNER_SIZE / 2;
-const RADIUS = CELLSIZE * 0.44;
-
 // MVCの初期化
 // 盤面MODEL生成
 const boardModel = new BoardModel();
